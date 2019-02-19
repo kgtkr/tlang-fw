e8d11629c5838985a4c07a2d81f5b828e6cf6681
kgtkr <kgtkr.jp@gmail.com>
2019-02-20 00:11:26 +0900

    ファイル分割

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+mod analyzer;
 mod lexer;
 mod stream;
 mod token;
--- src/stream.rs
+++ src/stream.rs
@@ -1,19 +1,17 @@
-use std::marker::PhantomData;
-
-struct Stream<T>(Vec<T>, usize);
+pub struct Stream<T>(Vec<T>, usize);
 
 impl<T: Clone> Stream<T> {
-    fn peak(&self) -> Option<T> {
+    pub fn peak(&self) -> Option<T> {
         self.0.get(self.1).cloned()
     }
 }
 
 impl<T> Stream<T> {
-    fn pos(&self) -> usize {
+    pub fn pos(&self) -> usize {
         self.1
     }
 
-    fn set_pos(&mut self, pos: usize) -> Option<()> {
+    pub fn set_pos(&mut self, pos: usize) -> Option<()> {
         if pos < self.0.len() {
             self.1 = pos;
             Some(())
@@ -22,73 +20,7 @@ impl<T> Stream<T> {
         }
     }
 
-    fn add_pos(&mut self, x: usize) -> Option<()> {
+    pub fn add_pos(&mut self, x: usize) -> Option<()> {
         self.set_pos(self.pos() + x)
-    }
-}
-
-trait Analyzer {
-    type Input;
-    type Output;
-    fn analyze(&self, stream: &mut Stream<Self::Input>) -> Option<Self::Output>;
-    fn map<T, F: Fn(Self::Output) -> T>(self, f: F) -> Map<T, Self, F>
-    where
-        Self: Sized,
-    {
-        Map::new(f, self)
-    }
-}
-struct AnyOne<T: Clone>(PhantomData<T>);
-
-impl<T: Clone> AnyOne<T> {
-    fn new() -> Self {
-        AnyOne(PhantomData)
-    }
-}
-
-impl<T: Clone> Analyzer for AnyOne<T> {
-    type Input = T;
-    type Output = T;
-    fn analyze(&self, st: &mut Stream<Self::Input>) -> Option<Self::Output> {
-        let val = st.peak()?;
-        st.add_pos(1);
-        Some(val)
-    }
-}
-
-struct Try<T: Analyzer>(T);
-
-impl<T: Analyzer> Try<T> {
-    fn new(x: T) -> Self {
-        Try(x)
-    }
-}
-
-impl<T: Analyzer> Analyzer for Try<T> {
-    type Input = T::Input;
-    type Output = T::Output;
-    fn analyze(&self, st: &mut Stream<T::Input>) -> Option<T::Output> {
-        let pos = st.pos();
-        let res = self.0.analyze(st);
-        if let None = res {
-            st.set_pos(pos);
-        }
-        res
-    }
-}
-
-struct Map<O, T: Analyzer, F: Fn(T::Output) -> O>(F, T, PhantomData<O>);
-
-impl<O, T: Analyzer, F: Fn(T::Output) -> O> Map<O, T, F> {
-    fn new(f: F, x: T) -> Self {
-        Map(f, x, PhantomData)
-    }
-}
-
-impl<O, T: Analyzer, F: Fn(T::Output) -> O> Analyzer for Map<O, T, F> {
-    type Input = T::Input;
-    type Output = O;
-    fn analyze(&self, st: &mut Stream<Self::Input>) -> Option<Self::Output> {
-        Some(self.0(self.1.analyze(st)?))
     }
 }
