acc14522a3f5095ee658936cff96dd5e1ba0d690
kgtkr <kgtkr.jp@gmail.com>
2019-02-19 16:26:30 +0900

    AnyOne,Try

--- src/stream.rs
+++ src/stream.rs
@@ -1,8 +1,46 @@
-struct Stream<I> {
-    data: Vec<I>,
-    pos: usize,
+struct Stream<T>(Vec<T>, usize);
+
+trait Analyzer {
+    type Input;
+    type Output;
+    fn analyze(&self, stream: &mut Stream<Self::Input>) -> Option<Self::Output>;
 }
 
-trait Analyzer<I, O> {
-    fn analyze(stream: &mut Stream<I>) -> O;
+struct AnyOne<T: Clone>(std::marker::PhantomData<T>);
+
+impl<T: Clone> AnyOne<T> {
+    fn new() -> Self {
+        AnyOne(std::marker::PhantomData)
+    }
+}
+
+impl<T: Clone> Analyzer for AnyOne<T> {
+    type Input = T;
+    type Output = T;
+    fn analyze(&self, Stream(data, pos): &mut Stream<T>) -> Option<T> {
+        let val = data.get(*pos).cloned()?;
+        *pos += 1;
+        Some(val)
+    }
+}
+
+struct Try<T: Analyzer>(T);
+
+impl<T: Analyzer> Try<T> {
+    fn new(x: T) -> Try<T> {
+        Try(x)
+    }
+}
+
+impl<T: Analyzer> Analyzer for Try<T> {
+    type Input = T::Input;
+    type Output = T::Output;
+    fn analyze(&self, st: &mut Stream<T::Input>) -> Option<T::Output> {
+        let pos = st.1;
+        let res = self.0.analyze(st);
+        if let None = res {
+            st.1 = pos;
+        }
+        res
+    }
 }
