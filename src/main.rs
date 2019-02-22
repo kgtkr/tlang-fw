e137310765e38c5e35dc4be6c6d588b65faf708d
kgtkr <kgtkr.jp@gmail.com>
2019-02-22 16:56:25 +0900

    テスト

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,8 @@
-fn main() {}
+use std::fs;
+use tkr_lang::{analyzer::Analyzer, lexer::lexer, stream::Stream};
+
+fn main() {
+    let s = fs::read_to_string("test").unwrap();
+    let mut st = Stream::new(s.chars().collect());
+    println!("{:?}", lexer().analyze(&mut st));
+}
