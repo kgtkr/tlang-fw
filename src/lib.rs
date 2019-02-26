cc4bcdd512f5164549c4e26f3fdf3c95f1c5d14e
kgtkr <kgtkr.jp@gmail.com>
2019-02-26 11:21:37 +0900

    block

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 #![feature(decl_macro)]
+#![feature(box_syntax)]
 
 pub mod analyzer;
 pub mod ast;
 pub mod lexer;
+pub mod parser;
 pub mod stream;
 pub mod token;
---
+++ src/parser.rs
@@ -0,0 +1,20 @@
+use crate::analyzer::{token, val, Analyzer};
+use crate::ast::Expr;
+use crate::token::{Kind, Symbol, Token};
+
+pub fn expr() -> impl Analyzer<Input = Kind, Output = Expr> {
+    unimplemented!();
+    val(Expr::I32Literal(1))
+}
+
+pub fn block() -> impl Analyzer<Input = Kind, Output = Expr> {
+    token(Kind::Symbol(Symbol::OpenBrace))
+        .with(
+            expr()
+                .skip(token(Kind::Symbol(Symbol::Semicolon)))
+                .attempt()
+                .many(),
+        )
+        .and(expr().optional())
+        .map(|(a, b)| Expr::Block(a, box b))
+}
