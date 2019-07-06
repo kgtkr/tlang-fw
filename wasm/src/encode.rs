a9c7317ee7c0ff43cd5b705604d74177a74e78b1
kgtkr <kgtkr.jp@gmail.com>
2019-07-06 17:17:00 +0900

    encode UintN

--- wasm/src/encode.rs
+++ wasm/src/encode.rs
@@ -1,3 +1,18 @@
+use byteorder::{LittleEndian, WriteBytesExt};
+
+
 trait BinaryEncode {
     fn encode(&self, bytes: &mut Vec<u8>);
+}
+
+fn encodeUint8(x: u8, bytes: &mut Vec<u8>) {
+    bytes.write_u8(x).unwrap();
+}
+
+fn encodeUint16(x: u16, bytes: &mut Vec<u8>) {
+    bytes.write_u16::<LittleEndian>(x).unwrap();
+}
+
+fn encodeUint32(x: u32, bytes: &mut Vec<u8>) {
+    bytes.write_u32::<LittleEndian>(x).unwrap();
 }
